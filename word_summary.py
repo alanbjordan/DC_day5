d65701d2537dee38d47737c7fd2acba290e66077
Alan Jordan <alan.jordanbiz@gmail.com>
2020-01-18 21:20:52 -0500

    |Dictionary Exercise| Word Summary: Updated instructions

--- word_summary.py
+++ word_summary.py
@@ -1,6 +1,6 @@
 #Letter Summary
-#Write a letter_histogram program that asks the user for input, and 
-#prints a dictionary containing the tally of how many times each letter in the alphabet was used in the word. 
+#Write a word_histogram program that asks the user for a sentence input, and 
+#prints a dictionary containing the tally of how many times each word in the sentence was used. 
 
 word = input("Enter a word: ").lower()
 split_word = word.split(" ")
